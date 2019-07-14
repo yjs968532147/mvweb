bb53778e547183c8c7d9ea794838a17da77e3b21
yjs968532147 <945906816@qq.com>
2019-07-14 15:46:43 +0800

    实现登录注册功能v2.0

--- src/main/java/com/yjs/mvweb/controller/UsersController.java
+++ src/main/java/com/yjs/mvweb/controller/UsersController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 /**
  * @Author: Administrator
  * @Date: Created in 11:29 2019/1/17
