35cda503895307553faf35b386ce435ead98b863
yjs968532147 <945906816@qq.com>
2019-07-16 14:26:44 +0800

    ver2.4

--- src/main/java/com/yjs/mvweb/controller/MainController.java
+++ src/main/java/com/yjs/mvweb/controller/MainController.java
@@ -1,14 +1,7 @@
 package com.yjs.mvweb.controller;
 
-import com.yjs.mvweb.pojo.Mv;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.enterprise.inject.Model;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/")
